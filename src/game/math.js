4d7c9f84ca7ff548b4da5f8f37ab76a212b0fea7
JedCov <jedcov@michaelhouse.org>
2026-05-11 19:14:51 +0200

    Make title mascot visibly pink

---
+++ src/game/math.js
@@ -0,0 +1,6 @@
+export const clamp = (value, min, max) => Math.max(min, Math.min(max, value));
+export const lerp = (a, b, t) => a + (b - a) * t;
+
+export function aabb(a, b) {
+  return a.minX <= b.maxX && a.maxX >= b.minX && a.minY <= b.maxY && a.maxY >= b.minY && a.minZ <= b.maxZ && a.maxZ >= b.minZ;
+}
